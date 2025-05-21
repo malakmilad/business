008ca840e9428edab55b15c60308415f72f383fe
Malak Milad <malakmilad202@gmail.com>
2025-05-21 22:58:42 +0300

    add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // No domains needed for local images
+    unoptimized: true, // OPTIONAL: disables Image Optimization if you want to use raw local files
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
